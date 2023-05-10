94231f1ff8fe2e27d2f4ad958cb42631c956c7e1
LucasSantamaria76 <luc_1976@hotmail.com>
2023-05-09 23:55:58 -0300

    fix modal new provider

--- client/src/Modals/ModalNewOrEditProvider.jsx
+++ client/src/Modals/ModalNewOrEditProvider.jsx
@@ -1,36 +1,37 @@
-import { Button, Grid, Modal, Text, TextInput, useMantineTheme } from '@mantine/core';
-import { useForm, zodResolver } from '@mantine/form';
-import { showNotification } from '@mantine/notifications';
-import { useCreateProviderMutation, useUpdateProviderMutation } from '../redux/apis/providers';
-import { providerSchema } from '../validationSchemas/providerSchema';
-import { showError, showSuccess } from '../utils/notifications';
+import { Button, Grid, Modal, Text, TextInput, useMantineTheme } from '@mantine/core'
+import { useForm, zodResolver } from '@mantine/form'
+import { showNotification } from '@mantine/notifications'
+import { useCreateProviderMutation, useUpdateProviderMutation } from '../redux/apis/providers'
+import { providerSchema } from '../validationSchemas/providerSchema'
+import { showError, showSuccess } from '../utils/notifications'
 
 const ModalNewOrEditProvider = ({ opened, setOpened, providerToEdit }) => {
-  const theme = useMantineTheme();
-  const [createProvider, { isLoading: isLoadingNew, error: errorNew }] = useCreateProviderMutation();
-  const [updateProvider, { isLoading: isLoadingEdit, error: errorEdit }] = useUpdateProviderMutation();
+  const theme = useMantineTheme()
+  const [createProvider, { isLoading: isLoadingNew, error: errorNew }] = useCreateProviderMutation()
+  const [updateProvider, { isLoading: isLoadingEdit, error: errorEdit }] =
+    useUpdateProviderMutation()
 
   const initialValues = !providerToEdit
     ? {
         nameProvider: '',
         address: '',
-        phone: '',
+        phone: ''
       }
     : {
         nameProvider: providerToEdit.nameProvider,
         address: providerToEdit.address,
-        phone: providerToEdit.phone,
-      };
+        phone: providerToEdit.phone
+      }
 
-  const labels = ['Nombre', 'Dirección', 'Teléfono'];
+  const labels = ['Nombre', 'Dirección', 'Teléfono']
 
-  const error = !providerToEdit ? errorNew : errorEdit;
-  const isLoading = !providerToEdit ? isLoadingNew : isLoadingEdit;
+  const error = !providerToEdit ? errorNew : errorEdit
+  const isLoading = !providerToEdit ? isLoadingNew : isLoadingEdit
 
   const FormProvider = useForm({
     initialValues,
-    validate: zodResolver(providerSchema),
-  });
+    validate: zodResolver(providerSchema)
+  })
 
   return (
     <Modal
@@ -42,18 +43,20 @@ const ModalNewOrEditProvider = ({ opened, setOpened, providerToEdit }) => {
       overlayOpacity={0.55}
       overlayBlur={3}
       shadow={'2px 2px 10px rgba(0,0,0,0.75)'}
-      centered>
+      centered
+    >
       <form
         onSubmit={FormProvider.onSubmit(async (values) => {
           !providerToEdit
             ? await createProvider(values).unwrap()
-            : await updateProvider({ ...values, id: providerToEdit.id }).unwrap();
+            : await updateProvider({ ...values, id: providerToEdit.id }).unwrap()
           if (!error) {
-            FormProvider.reset();
-            setOpened(false);
-            showNotification(showSuccess('Proveedor guardado con éxito'));
-          } else showNotification(showError('No se pudo guardar el Proveedor'));
-        })}>
+            FormProvider.reset()
+            setOpened(false)
+            showNotification(showSuccess('Proveedor guardado con éxito'))
+          } else showNotification(showError('No se pudo guardar el Proveedor'))
+        })}
+      >
         {Object.keys(initialValues).map((item, idx) => (
           <TextInput
             key={item}
@@ -65,22 +68,40 @@ const ModalNewOrEditProvider = ({ opened, setOpened, providerToEdit }) => {
             {...FormProvider.getInputProps(item)}
           />
         ))}
-        <Grid justify={'space-between'} align='flex-end'>
-          <Text size='xs' color={'pink'}>
+        <Grid
+          justify={'space-between'}
+          align='flex-end'
+        >
+          <Text
+            size='xs'
+            color={'pink'}
+          >
             * Campos requeridos
           </Text>
-          <Grid justify={'flex-end'} m={10}>
-            <Button type='button' color='pink' mr={10} onClick={() => setOpened(false)}>
+          <Grid
+            justify={'flex-end'}
+            m={10}
+          >
+            <Button
+              type='button'
+              color='pink'
+              mr={10}
+              onClick={() => setOpened(false)}
+            >
               Cancelar
             </Button>
-            <Button type='submit' variant='light' loading={isLoading}>
-              {isLoading ? 'guardando...' : 'Guardar producto'}
+            <Button
+              type='submit'
+              variant='light'
+              loading={isLoading}
+            >
+              {isLoading ? 'guardando...' : 'Guardar Proveedor'}
             </Button>
           </Grid>
         </Grid>
       </form>
     </Modal>
-  );
-};
+  )
+}
 
-export default ModalNewOrEditProvider;
+export default ModalNewOrEditProvider
